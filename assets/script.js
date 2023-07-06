9d6f1a0487c41dc9adc6406c496da340ae772ad4
Aaron <aaronlamfinance@gmail.com>
2023-07-06 00:04:47 -0700

    Add HTML + JS vars and functions

--- assets/script.js
+++ assets/script.js
@@ -3,39 +3,80 @@
 //GIVEN I am taking a code quiz
 
 //html selectors... ex. var startButtonEl=document.querySelector("#class-id")
-/*Global Variables 
-    Question Number:
-    Score: How much time you have left
-    Store time in seconds, set as 300 seconds. var time-left */
-var questionsArray = ["what is 1+1"]
-
+//Global Variables 
+    //Question Number:
+    //Score: How much time you have left
+    //Store time in seconds, set as 300 seconds. var time-left
     //array of answers[["Answer 1, Answer 2, Answer 3, Answer 4]] 
-    var answersArray=[]
-    var answerCorrect=[]
-                
-    ex.  answers[2][3] = Questions 2, answer C
+var startButton = document.querySelector("#start-Button");
+var timerEl = document.getElementByID('#timer-count');
+var winCounter = 0;
+var loseCounter = 0;
+var timeInterval;
+var timeLeft;
+var answerCorrect = addEventListener(click, "answer-correct");
+var answerIncorrect = addEventListener(click, "answer-incorrect");
+
+var questionsArray = [[""]]
+var answersArray=[["a. ,b. , c. ,d. "][""][""][""][""][""][""][""][""][""][""][""][""][""][""][""][""][""][""][""]]
+
+var answersCorrect=[[""][""][""][""][""][""][""][""][""][""][""][""][""][""][""][""][""][""][""][""]]
+
+
+
+function init() {
+    
+}   
+
+document.getElementById("start-button").addEventListener(
+    "click",
+    () => {
+        document.getElementById("start-button").hidden = true;
+        document.getElementById("start-show").hidden = true;
+        document.getElementById("questions").hidden = false;
+        document.getElementById("answers-list").hidden = false;
+        document.getElementById("timer-count").hidden = false;
+    },
+);
 
 /*WHEN I click the start button
 THEN a timer starts and I am presented with a question*/
 function startGame() {
+    timeLeft = 300;
     // Start button disappears
+    var clickStart = document.getElementById("button");
+    clickStart = 
+    }
     // Welcome text disappears
     // First question appears
             //Answers for first question appears
-    // Timer starts
-}
-
-function startTimer() {
+    // Timer starts 
     //set interval --
     // decrement the time left variable
     // check when interval = 0
+function startTimer() {
+timeInterval = setInterval(function(){
+    if (timeLeft > 0) {
+        timeLeft--;
+        timerEl.textContent = timeLeft + ' seconds remaining'; 
+    }
+    else (timeLeft === 0) {
+        timerEl.textContent = '';
+        clearInterval(timeInterval);
+        displayMessage("Game Over!")
+        }  
+    },1000);
 }
+
 
 function answerSelected() {
     //triggered by an event listener on the options
     //correct or wrong event.target = compare with elements in correct answersArray
     //if correct - correct - 
+    if
     //else message, incorrect
+    else 
+        timeLeft-10;
         //reduce time by 10 seconds
     //load the next question - so call function "newQuestion"
 }
@@ -54,8 +95,6 @@ THEN time is subtracted from the clock*/
 function incorrectAnswer() {
     // or use function newQuestion
     // reduce time by 10 seconds
-        
-
 }
 
 
@@ -81,6 +120,7 @@ function afterSubmit(){
 
 
 //add eventListener for 
+var start = addEventListener("button",startGame);
 //add eventListener for user triggering initials.
     //attach eventListner to var...
     //var submitInitials
